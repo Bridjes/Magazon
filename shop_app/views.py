7e11e32d86291f4e9ff8b4e189b6df2851d094cd
Bridjes <iluha.murzhin@gmail.com>
2023-06-18 01:04:08 +0300

    Создан файл БД, создана форма добавления записей в модель Items

--- shop_app/views.py
+++ shop_app/views.py
@@ -5,3 +5,7 @@ from .app import app, db
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/admin/item/add')
+def admin_item_add():
+    return render_template('admin_item_add.html')
