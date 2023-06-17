568bec2fd04b2ef6f0b10979dfd83d4d166ba53b
Bridjes <iluha.murzhin@gmail.com>
2023-06-17 20:59:02 +0300

    Убрал багулю с растяжением меню выбора языка на всю высоту

--- shop_app/app.py
+++ shop_app/app.py
@@ -3,6 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 # добавляем БД
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop_base.db'
 # объект БД
 db = SQLAlchemy(app)
